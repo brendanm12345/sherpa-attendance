2fd2f595b0978582de04dc5aed08d26bd24dc4cc
brendanmclaughlin <bmclaughlin0407@gmail.com>
2024-10-20 20:10:06 -0700

    rtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
-import DeployButton from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
@@ -20,8 +18,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "24/7 AI Attendance Assistant",
+  description: "Your own attendance assitant capable of automatically texting gaurdians about absences",
 };
 
 export const abcGinto = localFont({
--- app/notes/page.tsx
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@/utils/supabase/server';
-
-export default async function Notes() {
-    const supabase = createClient();
-    const { data } = await supabase.from("users").select();
-    console.log(data);
-
-    return <pre>{JSON.stringify(data, null, 2)}</pre>
-}
